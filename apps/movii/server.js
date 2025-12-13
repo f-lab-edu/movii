ee1725ddbc05780d0537d15815457d4f10a90278
Moon-Sangho <skym887@gmail.com>
2025-12-13 23:22:32 +0900

    chore: ec2 + pm2 + nginx 조합의 배포 방식으로 변경

--- apps/movii/server.js
+++ apps/movii/server.js
@@ -38,9 +38,26 @@ if (DEVELOPMENT) {
   });
 } else {
   console.log('Starting production server');
+  // css, js, webp 파일 등 정적 자산들은 1년 캐시
   app.use('/assets', express.static('dist/client/assets', { immutable: true, maxAge: '1y' }));
+  // HTTP 요청/응답 로그를 남기는 미들웨어. 정적 자산 요청은 로그에서 제외. (로그 노이즈 감소 + 성능 이점)
   app.use(morgan('tiny'));
-  app.use(express.static('dist/client', { maxAge: '1h' }));
+  app.use(
+    express.static('dist/client', {
+      // favicon, manifest, robots.txt 등 굳이 매 요청마다 받을 필요 없는 파일들
+      maxAge: '1h',
+      setHeaders: (res, path) => {
+        // dist/client 안의 모든 HTML은 캐시 금지
+        if (path.endsWith('.html')) {
+          res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+          // HTTP/1.0 구형 프록시/환경 대비
+          res.setHeader('Pragma', 'no-cache');
+          // HTTP/1.1에서는 Cache-Control 헤더가 우선하지만, HTTP/1.0/구형 캐시에서는 Expires를 참고
+          res.setHeader('Expires', '0');
+        }
+      },
+    }),
+  );
   app.use(await import(BUILD_PATH).then((mod) => mod.app));
 
   app.listen(PORT, () => {
